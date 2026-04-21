4c6ad8f28f53b972e1c6af3129039b86eac9c326
Dario262767 <dario.verdugo262767@potros.itson.edu.mx>
2026-04-20 22:21:13 -0700

    Commit receta ahi la lleva

--- dto_negocios/src/main/java/com/mycompany/dto_negocios/enums/Estado.java
+++ dto_negocios/src/main/java/com/mycompany/dto_negocios/enums/Estado.java
@@ -1,11 +1,11 @@
 package com.mycompany.dto_negocios.enums;
 
 /**
- *
+ * Enumerador que define los estados de las recetas.
  * @author Dario
  */
 public enum Estado {
     ACTIVA,
     SURTIDA,
-    CADACUDA
+    CADUCADA
 }
--- objetos_negocio/src/main/java/com/mycompany/objetos_negocio/RecetaBO.java
+++ objetos_negocio/src/main/java/com/mycompany/objetos_negocio/RecetaBO.java
@@ -1,12 +1,14 @@
 package com.mycompany.objetos_negocio;
 
 import com.mycompany.dto_negocios.RecetaDTO;
+import com.mycompany.dto_negocios.enums.Estado;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 /**
- *
+ * BO del subsistema de receta.
  * @author Dario
  */
 public class RecetaBO {
@@ -15,8 +17,6 @@ public class RecetaBO {
     public RecetaBO() {
         this.recetas = new ArrayList<>();
     }
-
-
 
     /**
      * Busca la receta recorriendo la lista uno por uno.
@@ -33,4 +33,56 @@ public class RecetaBO {
         }
         return null;
     }
+    
+    /**
+     * Reduce los usos de la receta y actualiza su estado.
+     * @param folio Folio de la receta a la que se le registrara un uso.
+     */
+    public void registrarUsoDeReceta(String folio) {
+        RecetaDTO receta = buscarPorFolio(folio);
+        if (receta != null && receta.getUsos() > 0) {
+            int nuevosUsos = receta.getUsos() - 1;
+            receta.setUsos(nuevosUsos);
+            
+            if (nuevosUsos == 0) {
+                receta.setEstado(Estado.SURTIDA);
+            } else {
+                receta.setEstado(Estado.ACTIVA); 
+            }
+        }
+    }
+    
+    /**
+     * Retorna el estado actual de la receta consultada.
+     * @param folio Folio de la receta para consultar.
+     * @return El estado de la receta o null.
+     */
+    public Estado obtenerEstadoDeReceta(String folio) {
+        RecetaDTO receta = buscarPorFolio(folio);
+        if (receta != null) {
+            return receta.getEstado();
+        }
+        return null;
+    }
+
+    /**
+     * Valida si la receta es apta para usarse basandose en fecha y estado actual.
+     * @param receta La receta que se calidara. 
+     * @return Si la receta se puede usar o no.
+     */
+    public boolean validarUsoReceta(RecetaDTO receta) {
+        if (receta == null){
+            return false;
+        } 
+        LocalDate hoy = LocalDate.now();
+        if (receta.getFechaCaducidad().isBefore(hoy)) {
+            receta.setEstado(Estado.CADUCADA);
+            return false;
+        }
+        if (receta.getEstado() == Estado.SURTIDA) {
+            return false;
+        }
+        return receta.getEstado() == Estado.ACTIVA;
+    }
+    
 }
