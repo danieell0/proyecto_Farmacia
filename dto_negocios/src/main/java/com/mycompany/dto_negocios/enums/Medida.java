6ccda1bea1f9070bdd6151b6b5cfe53d94299c32
Dario262767 <dario.verdugo262767@potros.itson.edu.mx>
2026-04-20 23:16:33 -0700

    Commit subsitema receta

--- dto_negocios/src/main/java/com/mycompany/dto_negocios/enums/Medida.java
+++ dto_negocios/src/main/java/com/mycompany/dto_negocios/enums/Medida.java
@@ -11,5 +11,4 @@ public enum Medida {
     gr,
     //mililitros
     ml,
-    
 }
--- negocios_receta/src/main/java/com/mycompany/negocios_receta/IReceta.java
+++ negocios_receta/src/main/java/com/mycompany/negocios_receta/IReceta.java
@@ -1,27 +1,27 @@
-package com.mycompany.objetos_negocio;
+package com.mycompany.negocios_receta;
 
 import com.mycompany.dto_negocios.RecetaDTO;
 import com.mycompany.dto_negocios.enums.Estado;
 
 /**
- * Interfaz que determina los metodos de la clase RecetaBO.
+ * Interfaz que determina los metodos de la clase FReceta
  * @author Dario
  */
-public interface IRecetaBO {
+public interface IReceta {
     /**
      * Busca la receta recorriendo la lista uno por uno.
      * @param folioBuscado El folio que el usuario ingresa.
      * @return La receta si el folio coincide, null si no.
      */
     public RecetaDTO buscarPorFolio(String folioBuscado)
-            throws NegocioException;
+            throws RecetaException;
     
     /**
      * Reduce los usos de la receta y actualiza su estado.
      * @param folio Folio de la receta a la que se le registrara un uso.
      */
     public void registrarUsoDeReceta(String folio)
-            throws NegocioException;
+            throws RecetaException;
     
     /**
      * Retorna el estado actual de la receta consultada.
@@ -29,7 +29,7 @@ public interface IRecetaBO {
      * @return El estado de la receta o null.
      */
     public Estado obtenerEstadoDeReceta(String folio)
-            throws NegocioException;
+            throws RecetaException;
 
     /**
      * Valida si la receta es apta para usarse basandose en fecha y estado actual.
@@ -37,5 +37,5 @@ public interface IRecetaBO {
      * @return Si la receta se puede usar o no.
      */
     public abstract boolean validarUsoReceta(RecetaDTO receta)
-            throws NegocioException;
+            throws RecetaException;
 }
